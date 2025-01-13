fc9add6374b0b8bf1fc3fbbf45bda9ce2872ed8c
Bercem Yildirim <bercemyildirim02@gmail.com>
2025-01-13 10:25:39 +0100

    DOM

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ const subTitle = "Alles over planeten";
 const planets = ["Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"];
 const routes = ["Home", "Universum", "Planeten"];
 
-
-
+//Opdracht 1: querySelector + textContent
+const h1Element = document.querySelector(".h1");
+h1Element.textContent = "Workshop";
 
 
 function addPlanet(){
@@ -13,3 +14,13 @@ function addPlanet(){
     const planetsView = document.querySelector(".plannitz");
     planetsView.innerHTML += "<li>" + planet + "</li>";
 }
+
+//Opdracht 2: querySelectorAll + for-loop
+const olElement = document.querySelector(".planets")
+olElement.innerHTML = "";
+for (let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    console.log(planet);
+    olElement.innerHTML += `<li> ${planet} </li>`;
+}
+
